e938e0f0bb508769e8038785749eba6a6f03c2aa
Martin Volerich <martin@volerich.com>
2015-01-10 09:41:42 -0500

    Update event_calendar.rb
    Fix scoped where

--- lib/event_calendar.rb
+++ lib/event_calendar.rb
@@ -49,11 +49,10 @@ module EventCalendar
     
     # Get the events overlapping the given start and end dates
     def events_for_date_range(start_d, end_d, find_options = {})
-      self.all.all(find_options).find(
-        :all,
-        :conditions => [ "(? <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< ?)", start_d.to_time.utc, end_d.to_time.utc ],
-        :order => "#{self.quoted_table_name}.#{self.start_at_field} ASC"
-      )
+      self.where(
+         "(:from_time <= #{self.quoted_table_name}.#{self.end_at_field}) AND (#{self.quoted_table_name}.#{self.start_at_field}< :end_time)",
+          {from_time: start_d.to_time.utc, end_time: end_d.to_time.utc })
+      .order( "#{self.quoted_table_name}.#{self.start_at_field} ASC")
     end
     
     # Create the various strips that show events.
